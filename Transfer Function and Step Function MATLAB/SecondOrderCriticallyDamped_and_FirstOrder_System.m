cdfab4cdafbc806d52f32f3f7e43792692994001
Mark Jefferson Garcia <158303837+IamImji@users.noreply.github.com>
2024-05-13 16:44:16 +0800

    Add files via upload

---
+++ Transfer Function and Step Function MATLAB/SecondOrderCriticallyDamped_and_FirstOrder_System.m
@@ -0,0 +1,19 @@
+%% 3. Second Order Critically Damped System 
+
+% Transfer Function G(s) = 1 / s^2 + 2s + 1
+numerator3 = [1];
+denominator3 = [1 2 1];
+TF3 = tf(numerator3, denominator3)
+
+
+%% 4. First Order System
+
+% Transfer Function G(s) = 1 / s + 1
+numerator4 = [1];
+denominator4 = [1 1];
+TF4 = tf(numerator4, denominator4)
+
+%% step response
+step(TF4,0:0.1:30)
+figure
+step(TF3,0:0.1:30)
